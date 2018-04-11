1a5ea5ac5323250fd5797b9366222d6264029069
Robson Farias <fariasrobson.r@gmail.com>
2018-04-10 20:37:05 -0700

    add component for counter

---
+++ stories/index.js
@@ -0,0 +1,6 @@
+import React from 'react'
+import Timer from '../src/components/Timer.js';
+import { storiesOf } from '@storybook/react';
+
+
+storiesOf('Timer', module).add('Show', () => <Timer />);
