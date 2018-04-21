779d71428c629091779063ecc7ba07514e2965b2
Robson Farias <fariasrobson.r@gmail.com>
2018-04-21 16:54:57 -0700

    react shopping cart

--- src/App.js
+++ src/App.js
@@ -1,18 +1,51 @@
 import React, { Component } from 'react';
-import logo from './logo.svg';
 import './App.css';
+
+import CartHeader from './components/CartHeader'
+import CartFooter from './components/CartFooter'
+import CartItems from './components/CartItems'
 
 class App extends Component {
   render() {
+    const copyright = <span>&copy; 2018</span>;
+    const cartItemsList = [
+  {
+    id: 1,
+     product: { id: 40,
+     name: 'Mediocre Iron Watch',
+     priceInCents: 399
+   },
+    quantity: 1
+    },
+  {
+    id: 2,
+    product: { id: 41, name: 'Heavy Duty Concrete Plate',
+    priceInCents: 499
+   },
+    quantity: 2
+    },
+  {
+    id: 3,
+     product: { id: 42, name: 'Intelligent Paper Knife',
+      priceInCents: 1999
+    },
+    quantity: 1
+    },
+    {
+      id:4,
+      product: {
+      id:43,
+      name: 'Sarcastic Glass Cup',
+      priceInCents: 1099
+    },
+    quantity: 3
+    }
+];
     return (
-      <div className="App">
-        <header className="App-header">
-          <img src={logo} className="App-logo" alt="logo" />
-          <h1 className="App-title">Welcome to React</h1>
-        </header>
-        <p className="App-intro">
-          To get started, edit <code>src/App.js</code> and save to reload.
-        </p>
+      <div class ClassName="App">
+        <CartHeader />
+        <CartItems cartItemsList={cartItemsList}/>
+        <CartFooter copyright={copyright}/>
       </div>
     );
   }
